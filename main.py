6cf0aac4cf4ccbb6716310270998578eaa1c3943
Frytasekjaktam <92473596+Frytasekjaktam@users.noreply.github.com>
2022-12-04 16:31:44 +0100

    Server
    get drinks and drinks by id

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description': drink.description}
+        output.append(drink_data)
+    return {"drinks": output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return {"name": drink.name, "description": drink.description}
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
